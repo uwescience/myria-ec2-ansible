c9f444a6b9a3d2c2d7a6e5d4da67f5952e122e89
Tobin Baker <tdbaker@cs.washington.edu>
2016-06-14 16:01:14 -0700

    final changes for PyPI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # myria-ec2-ansible
+
 Python package for deploying Myria on Amazon EC2
+
 ## How to set up a Myria cluster on EC2
 
 For local development, clone this repository, set up a virtual environment using `virtualenv`, and use the following command (run from the repository root directory) to install the package while allowing editing _in situ_:
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
     name='myria-cluster',
@@ -10,13 +12,13 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    author= 'Tobin Baker',
-    author_email= 'tdbaker@cs.washington.edu',
-    url= 'https://github.com/uwescience/myria-ec2-ansible/tarball/0.1.0',
-    license= 'BSD',
-    description= 'CLI to deploy the Myria parallel database on Amazon EC2',
-    long_description= readme(),
-    setup_requires = [
+    author='Tobin Baker',
+    author_email='tdbaker@cs.washington.edu',
+    url='https://github.com/uwescience/myria-ec2-ansible/tarball/0.1.0',
+    license='BSD',
+    description='CLI to deploy the Myria parallel database on Amazon EC2',
+    long_description=long_description,
+    setup_requires=[
         'setuptools_git >= 1.1',
     ],
     install_requires=[
